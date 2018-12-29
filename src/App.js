843b7a674743b48145e44ba1aed110d36fa0510e
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-30 01:45:05 +0200

    made component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-/*eslint-disable */
+import React, { Component, Suspense, lazy } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import './App.sass';
+import InvoicesPage from './pages/InvoicesPage';
+import data from './data/db.json';
+const CreateInvoice = lazy(() => import('./pages/CreateInvoicePage'));
+
+const invoices = data.invoices;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      invoices: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchInvoices();
+  }
+
+  fetchInvoices = () => {
+    this.setState({
+      invoices
+    });
+  };
+
+  addInvoice = invoice => {
+    this.setState(({ invoices }) => ({
+      invoices: [...invoices, invoice]
+    }));
+  };
+
   render() {
+    const { invoices } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route exact path="/" render={() => <InvoicesPage invoices={invoices} />} />
+          <Route
+            path="/create"
+            render={props => <CreateInvoice {...props} addInvoice={this.addInvoice} />}
+          />
+        </Suspense>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
