4cdfc430d706b57ebd837b2009c8b521f2f4abed
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-30 01:42:43 +0200

    added another file to export

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Actions } from './Actions/Actions';
 export { default as InvoicesTable } from './InvoicesTable/InvoicesTable';
+export { default as PageTitle } from './PageTitle/PageTitle';
