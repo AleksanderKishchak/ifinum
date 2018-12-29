2c11caaf0d43dd3a459526fbbeac3f023173ee00
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-30 01:44:33 +0200

    cut PageTitle in to a separate component

--- src/pages/InvoicesPage.js
+++ src/pages/InvoicesPage.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Actions,
-  InvoicesTable
-} from '../components';
+import { Actions, InvoicesTable, PageTitle } from '../components';
 
 /*eslint-disable */
 class InvoicesPage extends Component {
@@ -10,17 +7,8 @@ class InvoicesPage extends Component {
     const { invoices } = this.props;
 
     return (
-      <div className="invoices-page">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-12">
-              <div className="title">
-                <span>Invoices</span>
-                <span className="line"></span>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="invoices-page page">
+        <PageTitle title="Invoices" />
         <Actions />
         <InvoicesTable invoices={invoices} />
       </div>
